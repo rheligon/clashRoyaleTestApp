a1d4e8d7d814f5e74be1f45945ba726f9baeac38
Roberto Heligon <rheligon@wbinnova.com>
2019-06-08 19:08:51 -0500

    Initial Commit

---
+++ clashRoyaleTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clashRoyaleTestApp
+//
+//  Created by WBI Innovations LLC on 6/8/19.
+//  Copyright © 2019 WBI Innovations LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
